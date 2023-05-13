900e33f1b987d05a2d55f21706cf8128291fb382
pinchoboo <pinchoboo@gmail.com>
2023-05-13 13:27:49 +0200

    fix parser error with >=

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
@@ -6,10 +9,14 @@ mod parser;
 mod typecheck;
 
 fn main() {
-    match parser::parse_program("fn int main(){
-		int a = 1 * 2 + 2 * 3
-	}") {
-        Ok(p) => {dbg!(p);},
-        Err(e) => {dbg!(e);},
-    };
+    let path = "./main.lang";
+    let mut file = File::open(path).expect("file to exist");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("no problems reading the file");
+    let ast = parser::parse_program(&contents).expect("no parsing problems");
+    File::create(path.to_owned() + ".ast")
+        .expect("no problems opening/creating file")
+        .write_all(format!("{:#?}", ast).as_bytes())
+        .expect("no problems writing to the file");
 }
